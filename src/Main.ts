90f5d0621abc505bea609b68445e6c9624ab54e1
Kimchhik Chhuong <kimchhik.chhuong@student.passerellesnumeriques.org>
2025-05-30 23:24:17 +0700

    add main

--- src/Main.ts
+++ src/Main.ts
@@ -1,2 +1,12 @@
 import { Book } from "./Book";
 import { BorrowedBook } from "./BorrowedBook";
+
+let borrowed = new BorrowedBook(
+    'MemberA',
+    'BookA',
+    new Date('2024-05-01'), // borrow date
+    new Date('2024-05-10'), // due date
+    new Date('2024-05-15')  // return date
+);
+
+console.log("Fine amount: $" + borrowed.CalulateFine(new Date('2024-05-15')));
