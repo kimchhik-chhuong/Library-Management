42e7a8593bb7abe57bfe7ac12d8fb0ccad640109
kimchhik-chhuong <kimchhik.chhuong@gmail.com>
2025-05-30 14:12:53 +0700

    add method borrow

--- src/BorrowBook.ts
+++ src/BorrowBook.ts
@@ -4,6 +4,21 @@ export class BorrowBook {
         private borrowdate: Date,
         private duedate: Date,
         private returndate: Date,
-        private fine: number)
+        private fine= 0)
         {}
+
+        CalulateFine(): number{
+            const lateDays = Math.ceil(
+            (this.returndate.getTime() - this.duedate.getTime()) / (1000 * 3600 * 24)
+        );
+
+        if (lateDays > 0) {
+            const finePerDay = 10; // You can change this as needed
+            this.fine = lateDays * finePerDay;
+        } else {
+            this.fine = 0;
+        }
+
+        return this.fine;
+    }
 }
