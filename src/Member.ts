77f3649da282b723511a39e58bb8308bca04580d
kimchhik-chhuong <kimchhik.chhuong@gmail.com>
2025-05-30 15:36:53 +0700

    add pay

--- src/Member.ts
+++ src/Member.ts
@@ -1,33 +1,8 @@
-<<<<<<< HEAD
-// import { User } from './User';
-// import { BorrowBook } from './BorrowBook';
-// import { Review } from './Review';
-// // import { Book } from './Book';
-// // import { Fine } from './Fine';
-
-// export class Member extends User {
-//   constructor(
-//     public id: string,
-//     public fullName: string,
-//     public email: string,
-//     public phone: string,
-//     public address: string,
-//     public borrowBooks: BorrowBook[] = [],
-//     public reviews: Review[] = []
-//   ) {
-//     super();
-//   }
-=======
-<<<<<<< HEAD
-// import { User } from './User';
-import { BorrowedBook } from './BorrowedBook';
-=======
 import { User } from './User';
-import { BorrowBook } from './BorrowBook';
->>>>>>> 82319c145d9c8a40e98fd7d6b2aba95fbc827c6c
+import { BorrowedBook } from './BorrowedBook';
 import { Review } from './Review';
-// import { Book } from './Book';
-// import { Fine } from './Fine';
+import { Book } from './Book';
+import { Fine } from './Fine';
 
 export class Member extends User {
   constructor(
@@ -36,65 +11,29 @@ export class Member extends User {
     public email: string,
     public phone: string,
     public address: string,
-    public borrowBooks: BorrowBook[] = [],
+    public borrowBooks: BorrowedBook[] = [],
     public reviews: Review[] = []
   ) {
     super();
   }
 
-  borrowBook(book: Book): BorrowBook {
-    const borrowed = new BorrowBook(this, book, new Date(), new Date(), new Date(), new Fine());
+  borrowBook(book: Book): BorrowedBook {
+    const borrowed = new BorrowedBook(this, book, new Date(), new Date(), new Date(), new Fine());
     this.borrowBooks.push(borrowed);
     return borrowed;
   }
->>>>>>> main
 
-//   borrowBook(book: Book): BorrowBook {
-//     const borrowed = new BorrowBook(this, book, new Date(), new Date(), new Date(), new Fine());
-//     this.borrowBooks.push(borrowed);
-//     return borrowed;
-//   }
-
-<<<<<<< HEAD
-//   returnBook(): Fine {
-//     return new Fine();
-//   }
-=======
-  viewBorrowBooks(): BorrowBook[] {
+  viewBorrowBooks(): BorrowedBook[] {
     return this.borrowBooks;
   }
->>>>>>> main
 
-//   viewBorrowBooks(): BorrowBook[] {
-//     return this.borrowBooks;
-//   }
 
-<<<<<<< HEAD
-//   reviewBook(book: Book, rating: number, comment: string): Review {
-//     const review = new Review(this, book, rating, comment, new Date());
-//     this.reviews.push(review);
-//     return review;
-//   }
-=======
    getId(): number {
     return parseInt(this.id);
   }
->>>>>>> main
 
-//    getId(): number {
-//     return parseInt(this.id);
-//   }
 
-<<<<<<< HEAD
-//   getName(): string {
-//     return this.fullName;
-//   }
-// }
-=======
+  getName(): string {
+    return this.fullName;
+  }
 }
-
-<<<<<<< HEAD
-=======
-}
->>>>>>> 82319c145d9c8a40e98fd7d6b2aba95fbc827c6c
->>>>>>> main
