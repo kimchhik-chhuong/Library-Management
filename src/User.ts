308f8e125470a029c6692816112d6b6b630fec6f
zxally <channy789btb@gmail.com>
2025-05-30 16:12:27 +0700

    pull resolve

--- src/User.ts
+++ src/User.ts
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 export abstract class User {
   abstract getId(): number;
   abstract getName(): string;
 }
-=======
-export class User{
-    
-}
->>>>>>> 77f3649da282b723511a39e58bb8308bca04580d
