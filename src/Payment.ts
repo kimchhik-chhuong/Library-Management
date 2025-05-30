f4ac81dc37979823c74204b0795a0369bbcdb01f
kimchhik chhuong <kimchhik.chhuong@student.passerellesnumeriques.org>
2025-05-30 16:08:03 +0700

    add method payment

--- src/Payment.ts
+++ src/Payment.ts
@@ -1,9 +1,18 @@
-export class Payment {
+import { BorrowedBook } from "./BorrowedBook";
+
+export class Payment{
     constructor(
         private amount: number,
         private paymentdate: Date,
-        private paid: boolean
-    ){}
+        private paid: boolean,
+        // book: string,
+        // borrowdate: Date,
+        // duedate: Date,
+        // returndate: Date,
+        // fine= 0
+    ){
+        // super(book, borrowdate, duedate, returndate, fine);
+    }
 
     public getPaymentDetail(): string{
         const status = this.paid ? 'Paid' : 'Not Paid';
