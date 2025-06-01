a2e3930ab11a8379b33c7b76b6325c68009aa31c
zxally <channy789btb@gmail.com>
2025-06-01 12:40:06 +0700

    add new abstrac method

--- src/User.ts
+++ src/User.ts
@@ -1,4 +1,7 @@
 export abstract class User {
   abstract getId(): number;
   abstract getName(): string;
+  abstract login(email: string, password: string): boolean;
+  abstract logout(): void;
+  abstract updateProfile(info: object): void;
 }
